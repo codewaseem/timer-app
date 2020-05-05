5d9182b4e1102806f5b6a8f8279c592ff4c1b869
Waseem Ahmed <codewaseem@gmail.com>
2020-05-05 14:53:15 +0530

    ui v-0.0.1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,12 +4,20 @@ import styled from "@emotion/styled";
 import { Layout } from "~/components/Layout";
 import { SEO } from "~/components/SEO";
 import { StaticColors } from "../@types/emotion";
-import { WorkIcon } from "../components/Icons/Index";
+import {
+  WorkIcon,
+  CoffeeIcon,
+  LunchIcon,
+  LeaveIcon,
+  ExitIcon,
+} from "../components/Icons/Index";
 import Timer from "../components/Timer";
 
 const Container = styled.div`
   padding: 36px;
   height: 100%;
+  max-width: 768px;
+  margin: auto;
 `;
 
 const GreetingText = styled.h1`
@@ -17,7 +25,8 @@ const GreetingText = styled.h1`
   font-size: 2rem;
   line-height: 2rem;
   padding-bottom: 24px;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.text};
+  border-bottom: 1px solid ${({ theme }) => theme.staticColors.limeGreen};
+  color: ${({ theme }) => theme.colors.text};
 `;
 
 const HomeTilesContainer = styled.div`
@@ -41,6 +50,8 @@ const Card = styled.div<{
     background: ${({ theme, topColor }) =>
       theme.staticColors[topColor || "limeGreen"]};
   }
+  background-color: ${({ theme }) => theme.colors.cardBackground};
+
   border-radius: 3px;
   box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.1);
   padding: 20px;
@@ -58,6 +69,20 @@ const Title = styled.div`
   color: ${({ theme }) => theme.colors.textAccent};
 `;
 
+const ActionButton = styled.button`
+  border: 1px solid ${({ theme }) => theme.staticColors.limeGreen};
+  color: ${({ theme }) => theme.colors.text};
+  border-radius: 5px;
+  background-color: ${({ theme }) => theme.colors.background};
+  padding: 16px 32px;
+  font-weight: bold;
+  font-size: 1.1rem;
+  &:hover {
+    transform: translateY(-1px);
+    box-shadow: 0 3px 7px -2px rgba(0, 0, 0, 0.1);
+  }
+`;
+
 const Index: React.FC<InjectedIntlProps> = () => {
   const [isChecked, setChecked] = useState(false);
 
@@ -73,19 +98,60 @@ const Index: React.FC<InjectedIntlProps> = () => {
               <WorkIcon /> Work
             </Title>
             <Timer
+              isActive={false}
+              toggleActive={() => {}}
+              time={{
+                hours: 2,
+                minutes: 37,
+                seconds: 25,
+              }}
+              summaryText="Worked Today"
+            />
+          </Card>
+          <Card>
+            <Title>
+              <LunchIcon /> Lunch
+            </Title>
+            <Timer
               isActive={isChecked}
               toggleActive={toggleChecked}
               time={{
                 hours: 0,
-                minutes: 5,
-                seconds: 25,
+                minutes: 15,
+                seconds: 28,
               }}
+              summaryText="Lunch Time"
             />
           </Card>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card>
+            <Title>
+              <CoffeeIcon /> Coffee
+            </Title>
+            <Timer
+              isActive={false}
+              toggleActive={() => {}}
+              time={{
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+              }}
+              summaryText="Coffee Time"
+            />
+          </Card>
+          <Card>
+            <Title>
+              <LeaveIcon /> Leave
+            </Title>
+
+            <ActionButton>Take a Leave</ActionButton>
+          </Card>
+          <Card>
+            <Title>
+              <ExitIcon /> Exit
+            </Title>
+
+            <ActionButton>Log Off</ActionButton>
+          </Card>
         </HomeTilesContainer>
       </Container>
     </Layout>
